400ffa8f06dce177e296cfd020b7580fab45d13d
DavidC <dav.capuano98@gmail.com>
2023-06-10 19:21:52 +0200

    adding docker compose & minor fix

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ COPY requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY ./app /code/app
+COPY . /code
 
-#CMD [ "uvicorn" "app.main:app" "--host", "0.0.0.0" "--port" "80" ]
+EXPOSE 2323
 
-CMD [ "waitress-serve", "--listen=*:80", "app.main:app" ] 
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -2,9 +2,6 @@ import sys
 sys.path.append('.')
 
 from flask import Flask, render_template
-import subprocess, shlex
-import docker
-
 from util.mapGenerator import MapGenerator
 
 app = Flask(__name__)
@@ -19,24 +16,4 @@ def goToMap():
 
 if __name__ == "__main__":
 
-    # Check if MapTiler/Server Docker image is running
-    CONTAINER_NAME = "maptiles_server"
-    IMAGE_NAME = "maptiler/tileserver-gl"
-
-    docker_client = docker.from_env()
-
-    try :
-        container = docker_client.containers.get(CONTAINER_NAME)
-
-    except docker.errors.NotFound as ex:
-        #TODO check here what is the cwd 
-        args = shlex.split(r"docker run --name maptiles_server --rm -it -v C:\\Users\\David\\Git\\BeviVedovelle\\app\\static\\assets\\maptiles:/data -p 3065:80 maptiler/tileserver-gl:v4.1.2 --mbtiles 2017-07-03_italy_milan.mbtiles")
-        docker_container = subprocess.Popen(args)
-
-    # Getting docker container IP address
-    container = docker_client.containers.get(CONTAINER_NAME)
-    ipAddress = container.attrs['NetworkSettings']['IPAddress']
-    MILAN_COORDINATE = [45.465225, 9.186344]
-    # MapGenerator.createMap(location=MILAN_COORDINATE, maptilesServerIp='192.168.1.39')
-
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=2323)
